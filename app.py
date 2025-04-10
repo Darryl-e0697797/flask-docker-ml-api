2b10805062ee2eb7e1413f1f5dfbff7082904b6c
Darryl Lee <e0697797@u.nus.edu>
2025-04-10 20:55:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,17 +3,20 @@ from sklearn.linear_model import LinearRegression
 import numpy as np
 app = Flask(__name__)
 # Training data
-X = np.array([[0], [1], [2], [3], [4]])
-y = np.array([0, 2, 4, 6, 8])
+X = np.array([[0], [1], [1], [1], [0], [1], [1], [0], [0], [1], [1], [0], [0], [1], [0], [1], [0], [0], [1], [1]])
+w = np.array([[19.8], [23.4], [27.7], [24.6], [21.5], [25.1], [22.4], [29.3], [20.8], [20.2], [27.3], [24.5], [22.9], [18.4], [24.2], [21.0], [25.9], [23.2], [21.6], [22.8]])
+y = np.array([137, 118, 124, 124, 120, 129, 122, 142, 128, 114, 132, 130, 130, 112, 132, 117, 134, 132, 121, 128])
 model = LinearRegression().fit(X, y)
 @app.route("/predict")
 def predict():
   x = float(request.args.get("x", 0))
-  y_pred = model.predict([[x]])[0]
+  w = float(request.args.get("w", 0))
+  
+  y_pred = model.predict([[w, x]])[0]
   #  Log prediction
   with open("output.txt", "w") as f:
-    f.write(f"Input x: {x}\nPrediction: {y_pred}\n")
+    f.write(f"Input w: {w}, Input x: {x} \nPrediction: {y_pred}\n")
     
-  return jsonify({"x": x, "prediction": y_pred})
+  return jsonify({"w": w, "x": x, "prediction": y_pred})
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=5000)
